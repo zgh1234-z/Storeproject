15555f8224a7f668294ff265f61047b09f4efab6
zgh <ghahremans908@gmil.com>
2026-01-04 11:17:59 +0330

    delete file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-// import  "./components/slider/slider.jsx";
+// import slide "./components/slider/slider.jsx";
 // import Header from "./components/Header";
 
 function App() {
