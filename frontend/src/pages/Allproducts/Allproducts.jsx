3f744c72d2f5939eca5cb77e0198f2bc5d397349
zgh <ghahremans908@gmil.com>
2026-02-16 09:10:06 +0330

    .

--- frontend/src/pages/Allproducts/Allproducts.jsx
+++ frontend/src/pages/Allproducts/Allproducts.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Allproducts.css";
 import Header from "../../components/Header/app";
 import Footer from "../../components/Footer/Footer";
@@ -6,17 +6,34 @@ import Productstyle from "../../components/Productstyle/Productstyle";
 import crop from "../../components/Pageproducts/Pageproducts";
 
 function Allproducts() {
+  const [selectedCategory, setSelectedCategory] = useState([]);
+
+  let filter;
+
+  if (selectedCategory === "all") {
+    filter = products;
+  } else {
+    filter = products.filter((item) => item.category === selectedCategory);
+  }
+
   return (
     <div>
       <Header />
       <h2>همه محصولات </h2>
 
+      <input type="checkbox" name="چوبی" />
+
       <div className="All">
-        {crop.map((item) => {
-          <Productstyle item={item} />;
-        })}
+        {crop.map((item) => (
+          <Productstyle item={item} />
+        ))}
       </div>
-      <div className="filter"></div>
+      <div className="filter">
+        <input type="cheackbox"> چوبی </input>
+        <input type="cheackbox"> فرش </input>
+        <input type="cheackbox"> مسی </input>
+        <input type="cheackbox"> مجسمه سازی </input>
+      </div>
 
       <Footer />
     </div>
